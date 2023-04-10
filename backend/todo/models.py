4b90cc11561346d6ad33a611277cbf01d3dc8365
Ryan Martin <ryan@squadle.com>
2023-04-10 10:19:20 -0400

    added due date and date picker

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -6,6 +6,14 @@ class Todo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    due_date = models.DateField(blank=True, null=True)
+
+    class Priority(models.IntegerChoices):
+        LOW = 1
+        MEDIUM = 2
+        HIGH = 3
+
+    priority = models.IntegerField(choices=Priority.choices, default=1)
 
     def _str_(self):
         return self.title
